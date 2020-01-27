0d4598f0d13a84fb176c16f4754ac4163bcecc9c
Joojo Arthur <jarthur@nalosolutions.com>
2020-01-27 09:43:26 +0000

    commented data percentages in admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,22 +11,22 @@
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"> <i class="fa fa-clock-o"> </i> Users</span>
                 <div class="count">{{$lweek_users}}</div>
-                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$lweek_users / $total_users * 100}}% </i> From last Week</span>
+{{--                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$lweek_users / $total_users * 100}}% </i> From last Week</span>--}}
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Active Users</span>
                 <div class="count green">{{$active_users}}</div>
-                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$active_users / $total_users * 100}}% </i> </span>
+{{--                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$active_users / $total_users * 100}}% </i> </span>--}}
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> Inactive Users</span>
                 <div class="count red">{{$inactive_users}}</div>
-                <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>{{$inactive_users / $total_users * 100}}% </i> </span>
+{{--                <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>{{$inactive_users / $total_users * 100}}% </i> </span>--}}
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-user"></i> New Users</span>
                 <div class="count blue">{{$new_users}}</div>
-                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$new_users / $total_users * 100}}% </i> </span>
+{{--                <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$new_users / $total_users * 100}}% </i> </span>--}}
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                 <span class="count_top"><i class="fa fa-pencil-square"></i> Total Subscriptions</span>
@@ -41,22 +41,22 @@
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <span class="count_top"> <i class="fa fa-pencil-square"></i> New Subscriptions</span>
             <div class="count blue">{{$new_subs}}</div>
-            <span class="count_bottom"> <i class="green">{{$new_subs / $subs_counts * 100}}% </i> </span>
+{{--            <span class="count_bottom"> <i class="green">{{$new_subs / $subs_counts * 100}}% </i> </span>--}}
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <span class="count_top"> <i class="fa fa-pencil"> </i> Live Subscriptions</span>
             <div class="count green">{{$live_subs}}</div>
-            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$live_subs / $subs_counts * 100}}% </i> From last Week</span>
+{{--            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$live_subs / $subs_counts * 100}}% </i> From last Week</span>--}}
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <span class="count_top"><i class="fa fa-pencil"></i> Rejected Subscriptions</span>
             <div class="count red">{{$rej_subs}}</div>
-            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$rej_subs / $subs_counts * 100}}% </i> </span>
+{{--            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>{{$rej_subs / $subs_counts * 100}}% </i> </span>--}}
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <span class="count_top"><i class="fa fa-pencil"></i> Subscriptions</span>
             <div class="count">{{$active_subs}}</div>
-            <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>{{$active_subs / $subs_counts * 100}}% </i> To go live today</span>
+{{--            <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>{{$active_subs / $subs_counts * 100}}% </i> To go live today</span>--}}
         </div>
         <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
             <span class="count_top"><i class="fa fa-paypal"></i> Total Transactions</span>
