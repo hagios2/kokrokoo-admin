63b7c67fa8fce92280ee47de6bb4e23805edfa60
Joojo Arthur <jarthur@nalosolutions.com>
2020-02-10 13:07:44 +0000

    Fix view of subscription

--- resources/views/admin/subscriptions/subscriptions.blade.php
+++ resources/views/admin/subscriptions/subscriptions.blade.php
@@ -10,7 +10,7 @@
                     <input type="text" class="form-control pull-right" placeholder="search" id="search" style="border-radius: 15px;margin-bottom: -10px;">
                 </div>
                 <table id="laravel_datatable" class="table table-striped table-bordered table-responsive" style='font-family: "Open Sans",
-                 Roboto, Arial, "Droid Sans", sans-serif;font-size: 12px;color: rgb(35, 82, 124);font-weight: 100'>
+                 Roboto, Arial, "Droid Sans", sans-serif;font-size: 12px;color: gb(35, 82, 124);font-weight: 200'>
                     <thead>
                     <tr style="font-weight: bolder;color: #0b2e13">
                         <th>ID</th>
