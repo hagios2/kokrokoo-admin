6947cbc36a20ede8dc62a4e8ca27dacff72e9b9c
Joojo Arthur <jarthur@nalosolutions.com>
2020-02-10 13:25:19 +0000

    Fix view of subscription

--- resources/views/admin/subscriptions/subscriptions.blade.php
+++ resources/views/admin/subscriptions/subscriptions.blade.php
@@ -2,17 +2,16 @@
 @section('content')
         <div class="x_panel">
             <div class="x_title">
-                <h2 style="font-weight:700;line-height: 30px;font-size: 20px;">Subscriptions <small></small></h2>
+                <h2>Subscriptions <small></small></h2>
                 <div class="clearfix"></div>
             </div>
             <div class="x_content">
                 <div class="col-md-offset-9">
                     <input type="text" class="form-control pull-right" placeholder="search" id="search" style="border-radius: 15px;margin-bottom: -10px;">
                 </div>
-                <table id="laravel_datatable" class="table table-striped table-bordered table-responsive" style='font-family: "Open Sans",
-                 Roboto, Arial, "Droid Sans", sans-serif;font-size: 12px;color: gb(35, 82, 124);font-weight: 200'>
+                <table id="laravel_datatable" class="table table-striped table-bordered table-responsive">
                     <thead>
-                    <tr style="font-weight: bolder;color: #0b2e13">
+                    <tr>
                         <th>ID</th>
                         <th>Media house ID</th>
                         <th>Client ID</th>
