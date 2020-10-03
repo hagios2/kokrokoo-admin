bc9af0a1244043c67405ae18828870052f2257ab
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-10-03 06:33:17 +0000

    made changes

--- app/Models/ScheduledAd.php
+++ app/Models/ScheduledAd.php
@@ -7,46 +7,10 @@
 
 namespace App\Models;
 
-use Reliese\Database\Eloquent\Model as Eloquent;
+use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class ScheduledAd
- * 
- * @property int $id
- * @property string $subscription_id
- * @property string $client_id
- * @property string $media_house_id
- * @property string $rate_card_id
- * @property string $title
- * @property string $start
- * @property string $end
- * @property string $status
- * @property array $created_ad_data
- * @property string $file_name
- * @property string $file_type
- * @property string $file_path
- * @property string $file_size
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
- *
- * @package App\Models
- */
 class ScheduledAd extends Eloquent
 {
 	
-	protected $fillable = [
-		'subscription_id',
-		'client_id',
-		'media_house_id',
-		'rate_card_id',
-		'title',
-		'start',
-		'end',
-		'status',
-		'created_ad_data',
-		'file_name',
-		'file_type',
-		'file_path',
-		'file_size'
-	];
+	protected $guarded = ['id'];
 }
