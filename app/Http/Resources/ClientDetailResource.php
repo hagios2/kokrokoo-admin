401bc17a4eeb1b1a4082e4edd20764298b925bcd
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-10-08 23:49:40 +0000

    added new changes

--- app/Http/Resources/ClientDetailResource.php
+++ app/Http/Resources/ClientDetailResource.php
@@ -14,8 +14,83 @@ class ClientDetailResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
+        if(!$this->company)
+        {
+            return [
 
-        ];
+                'user' => [
+
+                    'id' => $this->id,
+
+                    'name' => $this->name,
+
+                    'email' => $this->email,
+
+                    'title' => $this->title,
+
+                    'phone1' => $this->phone1,
+
+                    'phone2' => $this->phone2,
+
+                    'role' => $this->role,
+
+                    'isActive' => $this->isAcitve,
+
+                    'last_login' => $this->last_login
+                ],
+
+            ];
+
+        }else{
+
+            return [
+
+                'user' => [
+
+                    'id' => $this->id,
+
+                    'name' => $this->name,
+
+                    'email' => $this->email,
+
+                    'title' => $this->title,
+
+                    'phone1' => $this->phone1,
+
+                    'phone2' => $this->phone2,
+
+                    'role' => $this->role,
+
+                    'isActive' => $this->isAcitve,
+
+                    'last_login' => $this->last_login
+
+                ],
+
+                'company' => [
+
+                    'id' => $this->company->id,
+
+                    'company_name' => $this->company->company_name,
+
+                    'company_profile' => $this->company->company_profile,
+
+                    'address' => $this->company->address,
+
+                    'website' => $this->company->website,
+
+                    'industry_type' => $this->company->industry_type,
+
+                    'business_cert' => $this->company->business_cert,
+
+                    'logo' => $this->company->avatar->logo,
+
+                    'company_email' => $this->company->company_email,
+
+                    'country' => $this->company->country
+
+                ],
+            ];
+        }
     }
 }
--- app/Http/Resources/MediaDetailResource.php
+++ app/Http/Resources/MediaDetailResource.php
@@ -14,6 +14,55 @@ class MediaDetailResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+
+            'user' => [
+
+                'id' => $this->id,
+
+                'name' => $this->name,
+
+                'email' => $this->email,
+
+                'title' => $this->title,
+
+                'phone1' => $this->phone1,
+
+                'phone2' => $this->phone2,
+
+                'role' => $this->role,
+
+                'isActive' => $this->isActive,
+
+                'last_login' => $this->last_login
+            ],
+
+            'company' => [
+
+                'id' => $this->company->id,
+
+                'company_name' => $this->company->company_name,
+
+                'media_house' => $this->company->media_house,
+
+                'media_type' => $this->company->mediaType->mediaType,
+
+                'address' => $this->company->address,
+
+                'website' => $this->company->website,
+
+                'purpose' => $this->company->purpose,
+
+                'operation_cert' => $this->company->operation_cert,
+
+                'business_cert' => $this->company->business_cert,
+
+                'logo' => $this->company->avatar->logo,
+
+                'isPublished' => $this->company->isPublished,
+
+                'hasBankDetails' => $this->company->bank ? true : false
+            ]
+        ];
     }
 }
