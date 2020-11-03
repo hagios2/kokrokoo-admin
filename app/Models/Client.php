b064684e513eb2c830d3bb0ac34e80e6a7c5e516
Emmanuel Oteng Wilson <ewilson@npontu.com>
2020-11-03 22:41:15 +0000

    added property role

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -15,4 +15,9 @@ class Client extends Model
         return $this->belongsTo('App\Models\Company');
     }
 
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
 }
