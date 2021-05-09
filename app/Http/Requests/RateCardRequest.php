7f6a2baf7e78a15358ddd9eec951cc9f89ccc122
Emmanuel Oteng Wilson <ewilson@npontu.com>
2021-05-09 03:25:18 +0000

    made changes

--- app/Http/Requests/RateCardRequest.php
+++ app/Http/Requests/RateCardRequest.php
@@ -22,7 +22,7 @@ class RateCardRequest extends FormRequest
      *
      * @return array
      */
-    public function rules(RateCardTitle $ratecard_title)
+    public function rules()
     {
         dd(request()->route()->parameter('company'));
 
