038cdae42671998783cb5df09f92c81f4fc9d642
Emmanuel Oteng Wilson <ewilson@npontu.com>
2021-05-09 04:36:23 +0000

    made changes

--- app/Models/RateCard.php
+++ app/Models/RateCard.php
@@ -1,22 +1,11 @@
 <?php
 
-/**
- * Created by Reliese Model.
- * Date: Sun, 20 Oct 2019 17:06:30 +0000.
- */
-
 namespace App\Models;
 
-use App\Duration;
 use Illuminate\Database\Eloquent\Model;
 
 class RateCard extends Model
 {
-	protected $casts = [
-		'segments' => 'json',
-		'weekend_segments' => 'json'
-	];
-
 	protected $guarded = ['id'];
 
     public function day()
